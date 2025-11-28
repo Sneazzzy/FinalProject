ea7d0370dab4216c99caf01045a1610a4155ce44
Sneazzzy <jamespestanas22@gmail.com>
2025-11-28 21:43:50 +0800

    Update signin.js

---
+++ signin.js
@@ -0,0 +1,20 @@
+document
+  .querySelector(".toggle-password")
+  .addEventListener("click", function () {
+    const input = this.parentElement.querySelector("input");
+    if (input.type === "password") {
+      input.type = "text";
+      this.textContent = "🙈";
+    } else {
+      input.type = "password";
+      this.textContent = "👁️";
+    }
+  });
+
+document.getElementById("signin").addEventListener("submit", function (event) {
+  event.preventDefault();
+  const emailOrNum = document.getElementById("emailornum").value.trim();
+  const password = document.getElementById("password").value.trim();
+
+
+});
